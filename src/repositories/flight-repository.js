857195f094054ca60f89b96c798429d1ad9ef2a5
prashant <prashantkumar2488@gmail.com>
2024-07-02 14:52:44 +0530

    FEAT: added create flight api

---
+++ src/repositories/flight-repository.js
@@ -0,0 +1,11 @@
+const crudRepository = require('./crud-repository');
+
+const { Flight } = require('../models');
+
+class FlightRepository extends crudRepository {
+    constructor() {
+        super(Flight);
+    }
+}
+
+module.exports = FlightRepository;
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -8,6 +8,8 @@ const cityRoutes = require('./city-routes');
 
 const airportRoutes = require('./airport-routes');
 
+const flightRoutes = require('./flight-routes');
+
 const router = express.Router();
 
 
@@ -16,6 +18,8 @@ router.use('/airplanes', airplaneRoutes);
 router.use('/cities',cityRoutes);
 
 router.use('/airports', airportRoutes);
+
+router.use('/flights',flightRoutes);
 
 router.get('/info',InfoController.info);
 
